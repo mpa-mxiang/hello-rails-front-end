7831aacf9a32ae31a4e78adcc3484a1997b98807
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-12-14 00:00:24 -0500

    delete all in main

--- src/store/index.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import greetingReducer from '../reducers';
-
-const store = createStore(greetingReducer, applyMiddleware(thunk));
-
-export default store;
